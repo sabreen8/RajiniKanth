d3e2e2c9767bc8b164f9c7f1080ba42496890293
Sabreen <sabreensheik@gmail.com>
2024-02-25 17:08:38 +0530

    Initial commit

---
+++ src/test/java/Testingp.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class Testingp {
+ @Test
+    void test1()
+    {
+        System.out.println("I am a good girl");
+    }
+@Test
+    void test2()
+    {
+        System.out.println("I am getting better day by day");
+
+    }
+@Test
+    void test3()
+    {
+        System.out.println("wish me all the very best");
+    }
+}
